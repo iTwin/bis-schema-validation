4cacfedcd213f1aa6b85204205e7d30e003be6c5
Bao Tran <38288322+imodeljs-admin@users.noreply.github.com>
2019-08-18 22:38:32 +0000

    Merged PR 40272: Backend XML deserialization
    Implement XML back end deserialization to support deserializing Schema EC3.2 and below.
    
    Various clean up to rename schema validater to schema validator.

---
+++ schema-validator/src/ValidationDiagnosticReporter.ts
@@ -0,0 +1,38 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* Licensed under the MIT License. See LICENSE.md in the project root for license terms.
+*--------------------------------------------------------------------------------------------*/
+
+import { FormatDiagnosticReporter, AnyDiagnostic, diagnosticCategoryToString } from "@bentley/ecschema-metadata";
+
+/**
+ * An IDiagnosticReporter implementation that formats diagnostics into a readable string
+ * and then calls the abstract method reportedFormattedDiagnostic for further processing.
+ */
+export abstract class ValidationDiagnosticReporter extends FormatDiagnosticReporter {
+
+  /** Template to use when creating the diagnostic message: 0-Category, 1-Code, 2-Message */
+  public static diagnosticMessageTemplate = `{0} {1}: {2}`;
+
+  /**
+   * Adds the given IDiagnostic to a collection.
+   * @param diagnostic The diagnostic to report.
+   */
+  public reportDiagnostic(diagnostic: AnyDiagnostic, messageText: string) {
+    const text = this.formatDiagnostic(diagnostic, messageText);
+    this.reportFormattedDiagnostic(text);
+  }
+
+  /**
+   * Allows the implementation to handle the formatted string representation of an IDiagnostic.
+   * @param message
+   */
+  protected abstract reportFormattedDiagnostic(message: string): void;
+
+  private formatDiagnostic(diagnostic: AnyDiagnostic, messageText: string) {
+    const category = diagnosticCategoryToString(diagnostic.category);
+    const args = [category, diagnostic.code, messageText];
+
+    return this.formatStringFromArgs(ValidationDiagnosticReporter.diagnosticMessageTemplate, args);
+  }
+}
---
+++ schema-validator/test/CollectionDiagnosticReporter.test.ts
@@ -0,0 +1,38 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* Licensed under the MIT License. See LICENSE.md in the project root for license terms.
+*--------------------------------------------------------------------------------------------*/
+
+import { expect } from "chai";
+import * as sinon from "sinon";
+import * as ECRules from "@bentley/ecschema-metadata/lib/Validation/ECRules";
+import * as EC from "@bentley/ecschema-metadata/lib/ecschema-metadata";
+import { CollectionDiagnosticReporter } from "../src/CollectionDiagnosticReporter";
+import { ValidationDiagnosticReporter } from "../src/ValidationDiagnosticReporter";
+import { diagnosticCategoryToString } from "@bentley/ecschema-metadata/lib/Validation/Diagnostic";
+
+describe("CollectionDiagnosticReporter Tests", () => {
+
+  function formatStringFromArgs(text: string, args: ArrayLike<string>, baseIndex = 0): string {
+    return text.replace(/{(\d+)}/g, (_match, index: string) => args[+index + baseIndex]);
+  }
+
+  it("report diagnostic, diagnostic added correctly.", async () => {
+    const reporter = new CollectionDiagnosticReporter();
+    const baseSpy = sinon.spy(reporter, "reportDiagnostic");
+    const schema = new EC.Schema(new EC.SchemaContext(), "TestSchema", 1, 0, 0);
+    const baseClass = new EC.EntityClass(schema, "BaseClass");
+    const testClass = new EC.EntityClass(schema, "TestClass");
+
+    const diagnostic = new ECRules.Diagnostics.BaseClassIsSealed(testClass, [testClass.fullName, baseClass.fullName]);
+    reporter.report(diagnostic);
+
+    const category = diagnosticCategoryToString(diagnostic.category);
+    const message = baseSpy.args[0][1];
+    const args = [category, diagnostic.code, message];
+    const formattedMsg = formatStringFromArgs(ValidationDiagnosticReporter.diagnosticMessageTemplate, args);
+
+    expect(reporter.diagnostics.length).to.equal(1);
+    expect(reporter.diagnostics[0]).to.equal(formattedMsg);
+  });
+});
