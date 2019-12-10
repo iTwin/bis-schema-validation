5f249f8e1181ac4c059321509a42bd3799580593
Chris Lawson <32881725+christophermlawson@users.noreply.github.com>
2019-12-10 22:08:17 +0000

    Merged PR 58479: add diag code to comparison results
    add diag code to comparison results
    
    Related work items: #224773

--- schema-comparer/test/CollectionSchemaCompareReporter.test.ts
+++ schema-comparer/test/CollectionSchemaCompareReporter.test.ts
@@ -5,7 +5,7 @@
 
 import { expect } from "chai";
 import { CollectionSchemaCompareReporter } from "../src/CollectionSchemaCompareReporter";
-import { SchemaCompareDiagnostics, SchemaChanges, Schema, SchemaContext } from "@bentley/ecschema-metadata/lib/ecschema-metadata";
+import { SchemaCompareDiagnostics, SchemaChanges, Schema, SchemaContext, SchemaCompareCodes } from "@bentley/ecschema-metadata/lib/ecschema-metadata";
 
 describe("CollectionSchemaCompareReporter Tests", () => {
   it("report diagnostic, diagnostic added correctly.", async () => {
@@ -19,7 +19,11 @@ describe("CollectionSchemaCompareReporter Tests", () => {
     reporter.report(schemaChanges);
 
     expect(reporter.changeMessages.length).to.equal(2);
-    expect(reporter.changeMessages[0]).to.equal(`!Schema(TestSchema)`);
-    expect(reporter.changeMessages[1]).to.equal(`!\tLabel: LabelA -> LabelB`);
+    expect(reporter.changeMessages[0].message).to.equal(`!Schema(TestSchema)`);
+    expect(reporter.changeMessages[0].change).to.be.undefined;
+    expect(reporter.changeMessages[1].message).to.equal(`!\tLabel: LabelA -> LabelB`);
+    const change = reporter.changeMessages[1].change;
+    expect(change).to.not.be.undefined;
+    expect(change!.diagnostic.code).to.equal(SchemaCompareCodes.SchemaDelta);
   });
 });
