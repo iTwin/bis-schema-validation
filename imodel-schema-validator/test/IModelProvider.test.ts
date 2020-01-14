76e984706f686d6a300b59043adbc0a2306c0eea
Naveed Khan <38525837+naveedkhan8067@users.noreply.github.com>
2020-01-14 05:50:11 +0000

    Merged PR 62133: Deprecating support for IMS and skipping ecv3conversionattributes schema from difference audit
    - Deprecating IMS as it will no longer used. It is replaced by OIDC.
    - Handling the schema-comparer tool issue by skipping the  difference audit for ecv3conversionattributes schema

--- imodel-schema-validator/test/IModelProvider.test.ts
+++ imodel-schema-validator/test/IModelProvider.test.ts
@@ -10,11 +10,9 @@ import * as rimraf from "rimraf";
 import { IModelHost } from "@bentley/imodeljs-backend";
 
 describe("IModelProvider Tests", async () => {
-  const imsUserName: any = process.env.imsUserName;
-  const imsPassword: any = process.env.imsPassword;
   const oidcUserName: any = process.env.oidcUserName;
   const oidcPassword: any = process.env.oidcPassword;
-  const env: any = process.env.environment;
+  const env = "QA";
   const tempDir: any = process.env.TMP;
   const briefcaseDir: string = path.join(tempDir, "SchemaValidation", "Briefcases", "validation");
 
@@ -28,36 +26,7 @@ describe("IModelProvider Tests", async () => {
     IModelHost.shutdown();
   });
 
-  it("Get iModel Id (IMS Auth), Get an iModel id by its name", async () => {
-    IModelProvider.setupHost(env, briefcaseDir);
-    const requestContext = await IModelProvider.imsConnect(imsUserName, imsPassword);
-    const iModelId: any = await IModelProvider.getIModelId(requestContext, "9cf0d519-0436-446b-83b4-182752c9a4eb", "validation");
-
-    expect(iModelId).to.equal("367ac967-9894-4434-842a-9d3557b3ddbd");
-  });
-
-  it("Wrong iModel name (IMS Auth), When user insert wrong iModel name.", async () => {
-    IModelProvider.setupHost(env, briefcaseDir);
-    const requestContext = await IModelProvider.imsConnect(imsUserName, imsPassword);
-    const iModelId: any = await IModelProvider.getIModelId(requestContext, "9cf0d519-0436-446b-83b4-182752c9a4eb", "val");
-
-    expect(iModelId, "undefined");
-  });
-
-  it("Export XML Schemas (IMS Auth), Get local iModel connection and export the schemas present in it.", async () => {
-    const iModelSchemaDir = await IModelProvider.exportSchemasFromIModel(true, "9cf0d519-0436-446b-83b4-182752c9a4eb", "validation", briefcaseDir, imsUserName, imsPassword, env);
-    let result = false;
-    if (iModelSchemaDir) {
-      fs.readdirSync(iModelSchemaDir).forEach((file) => {
-        if (file.toLowerCase().endsWith(".ecschema.xml")) {
-          result = true;
-        } else { result = false; }
-      });
-    }
-    expect(result).to.equal(true);
-  });
-
-  it.skip("Get iModel Id (OIDC Auth), Get an iModel id by its name", async () => {
+  it.skip("Get IModel Id, Get an iModel id by its name", async () => {
     IModelProvider.setupHost(env, briefcaseDir);
     const requestContext = await IModelProvider.oidcConnect(oidcUserName, oidcPassword, 102);
     const iModelId: any = await IModelProvider.getIModelId(requestContext, "9cf0d519-0436-446b-83b4-182752c9a4eb", "validation");
@@ -65,7 +34,7 @@ describe("IModelProvider Tests", async () => {
     expect(iModelId).to.equal("367ac967-9894-4434-842a-9d3557b3ddbd");
   });
 
-  it.skip("Wrong iModel name (OIDC Auth), When user insert wrong iModel name.", async () => {
+  it.skip("Wrong IModel Name, When user insert wrong iModel name.", async () => {
     IModelProvider.setupHost(env, briefcaseDir);
     const requestContext = await IModelProvider.oidcConnect(oidcUserName, oidcPassword, 102);
     const iModelId: any = await IModelProvider.getIModelId(requestContext, "9cf0d519-0436-446b-83b4-182752c9a4eb", "val");
@@ -73,8 +42,8 @@ describe("IModelProvider Tests", async () => {
     expect(iModelId, "undefined");
   });
 
-  it.skip("Export XML Schemas (OIDC Auth), Get local iModel connection and export the schemas present in it.", async () => {
-    const iModelSchemaDir = await IModelProvider.exportSchemasFromIModel(false, "9cf0d519-0436-446b-83b4-182752c9a4eb", "validation", briefcaseDir, oidcUserName, oidcPassword, env);
+  it.skip("Export XML Schemas, Get local iModel connection and export the schemas present in it.", async () => {
+    const iModelSchemaDir = await IModelProvider.exportSchemasFromIModel("9cf0d519-0436-446b-83b4-182752c9a4eb", "validation", briefcaseDir, oidcUserName, oidcPassword, env);
     let result = false;
     if (iModelSchemaDir) {
       fs.readdirSync(iModelSchemaDir).forEach((file) => {
