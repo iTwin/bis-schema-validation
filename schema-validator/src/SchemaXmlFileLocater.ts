56376cecf7e21cd393983e5bd6cf5d78ab063766
Chris Lawson <32881725+christophermlawson@users.noreply.github.com>
2020-02-13 20:40:18 +0000

    Merged PR 68162: Merge native-deserialization to master
    As in the past, you will see two copies of SchemaDeserializer and SchemaXmlFileLocater for the validater and comparer tools. You only need to review one set, as the are identical.
    
    Related work items: #274149

--- schema-validator/src/SchemaXmlFileLocater.ts
+++ schema-validator/src/SchemaXmlFileLocater.ts
@@ -5,10 +5,14 @@
 
 import * as path from "path";
 import { DOMParser } from "xmldom";
-import { ECSchemaXmlContext } from "@bentley/imodeljs-backend";
+import { ECSchemaXmlContext, SchemaKey as NativeSchemaKey } from "@bentley/imodeljs-backend";
 import { FileSchemaKey, SchemaFileLocater } from "@bentley/ecschema-locaters";
 import { ISchemaLocater, SchemaMatchType, Schema, SchemaKey, SchemaContext, SchemaReadHelper,
-  ECObjectsError, ECObjectsStatus, XmlParser, ECVersion } from "@bentley/ecschema-metadata";
+  ECObjectsError, ECObjectsStatus, XmlParser, ECVersion, SchemaGraphUtil } from "@bentley/ecschema-metadata";
+
+function isECv2Schema(schemaText: string): boolean {
+  return /<ECSchema[^>]*xmlns=".*ECXML.2.0"/.test(schemaText);
+}
 
 /**
  * A SchemaLocater implementation for locating XML Schema files
@@ -52,6 +56,17 @@ export class SchemaXmlFileLocater extends SchemaFileLocater implements ISchemaLo
    */
   public getSchemaSync<T extends Schema>(key: SchemaKey, matchType: SchemaMatchType, context: SchemaContext): T | undefined {
     matchType = this._matchType ? this._matchType : matchType;
+    return this.loadSchema(key, matchType, context);
+  }
+
+  /**
+   * Attempts to retrieve a Schema with the given SchemaKey by using the configured search paths
+   * to locate the XML Schema file from the file system.
+   * @param key The SchemaKey of the Schema to retrieve.
+   * @param matchType The SchemaMatchType.
+   * @param context The SchemaContext that will control the lifetime of the schema.
+   */
+  public loadSchema<T extends Schema>(key: SchemaKey, matchType: SchemaMatchType, context: SchemaContext): T | undefined {
     const candidates: FileSchemaKey[] = this.findEligibleSchemaKeys(key, matchType, "xml");
 
     if (!candidates || candidates.length === 0)
@@ -70,8 +85,10 @@ export class SchemaXmlFileLocater extends SchemaFileLocater implements ISchemaLo
 
     this.addSchemaSearchPaths([path.dirname(schemaPath)]);
 
-    if (this.isEC31Schema(schemaText) || this.isECv2Schema(schemaText))
-      return this.getSchemaFromNativeEnv(key, context, schemaPath) as T;
+    if (this.isEC31Schema(schemaText) || isECv2Schema(schemaText)) {
+      const refMatchType = this._matchType ? this._matchType : matchType;
+      return this.getSchemaFromNativeEnv(key, schemaText, schemaPath, context, refMatchType) as T;
+    }
 
     const parser = new DOMParser();
     const reader = new SchemaReadHelper(XmlParser, context);
@@ -95,7 +112,7 @@ export class SchemaXmlFileLocater extends SchemaFileLocater implements ISchemaLo
     }
 
     let ecVersion: ECVersion;
-    if (this.isECv2Schema(schemaXml))
+    if (isECv2Schema(schemaXml))
       ecVersion = SchemaXmlFileLocater.fromECv2String(match.groups.version);
     else
       ecVersion = ECVersion.fromString(match.groups.version);
@@ -121,27 +138,198 @@ export class SchemaXmlFileLocater extends SchemaFileLocater implements ISchemaLo
     return new ECVersion(+read, 0, +minor);
   }
 
-  private getSchemaFromNativeEnv(key: SchemaKey, context: SchemaContext, schemaPath: string): Schema {
+  private getSchemaFromNativeEnv(schemaKey: SchemaKey, schemaText: string, schemaPath: string, context: SchemaContext, refMatchType: SchemaMatchType): Schema {
+    const stubLocater = new StubSchemaXmlFileLocater();
+    stubLocater.addSchemaSearchPaths(this.searchPaths);
+    const schemaStub = stubLocater.loadSchema(schemaText, schemaPath, refMatchType);
+    const orderedSchemas = SchemaGraphUtil.buildDependencyOrderedSchemaList(schemaStub);
+
     const nativeContext = new ECSchemaXmlContext();
     for (const refPath of this.searchPaths) {
       nativeContext.addSchemaPath(refPath);
     }
 
-    try {
-      const schemaJson = nativeContext!.readSchemaFromXmlFile(schemaPath);
-      return Schema.fromJsonSync(schemaJson, context);
-    } catch (err) {
-      if (err.message === "ReferencedSchemaNotFound")
-        throw new ECObjectsError(ECObjectsStatus.UnableToLocateSchema, `Unable to load schema '${key.name}'. A referenced schema could not be found.`);
-      throw (err);
+    for (const currentStub of orderedSchemas) {
+      try {
+        // If schema is already in the context skip it
+        if (context.getCachedSchemaSync(currentStub.schemaKey, SchemaMatchType.Exact))
+          continue;
+
+        const xmlKey = currentStub.schemaKey as FileSchemaKey;
+        const schemaJson = nativeContext!.readSchemaFromXmlFile(xmlKey.fileName);
+        Schema.fromJsonSync(schemaJson, context);
+      } catch (err) {
+        if (err.message === "ReferencedSchemaNotFound")
+          throw new ECObjectsError(ECObjectsStatus.UnableToLocateSchema, `Unable to load schema '${schemaKey.name}'. A referenced schema could not be found.`);
+        throw (err);
+      }
     }
+
+    // Schema should always be found (!) as it was just added to the context above.
+    return context.getSchemaSync(schemaStub.schemaKey)!;
   }
 
   private isEC31Schema(schemaText: string): boolean {
     return /<ECSchema[^>]*xmlns=".*ECXML.3.1"/.test(schemaText);
   }
+}
 
-  private isECv2Schema(schemaText: string): boolean {
-    return /<ECSchema[^>]*xmlns=".*ECXML.2.0"/.test(schemaText);
+/**
+ * A SchemaLocater implementation for locating XML Schema files
+ * from the file system using configurable search paths. Returns only
+ * Schemas from XML files with their keys populated.
+ * @internal This is a workaround the current lack of a full xml parser.
+ */
+class StubSchemaXmlFileLocater extends SchemaFileLocater implements ISchemaLocater {
+  /**
+   * Loads a Schema from disk as a Promise.
+   * @param schemaPath The path to the Schema file.
+   * @param context The SchemaContext that will control the lifetime of the schema.
+   * @param refMatchType The SchemaMatchType to use when locating schema references.
+   */
+  public loadSchema(schemaText: string, schemaPath: string, refMatchType: SchemaMatchType): Schema {
+    this.addSchemaSearchPaths([path.dirname(schemaPath)]);
+    const key = this.getSchemaKey(schemaText);
+    const alias = this.getSchemaAlias(schemaText);
+    const context = new SchemaContext();
+    context.addLocater(this);
+
+    // Load the schema and return it
+    const schema = new Schema(context, new FileSchemaKey(key, schemaPath, schemaText), alias);
+    this.addSchemaReferences(schema, context, refMatchType);
+    return schema;
+  }
+
+  /**
+   * Attempts to retrieve a Schema with the given SchemaKey by using the configured search paths
+   * to locate the XML Schema file from the file system. Returns only Schemas from XML files with
+   * their keys populated.
+   * @param key The SchemaKey of the Schema to retrieve.
+   * @param matchType The SchemaMatchType.
+   * @param context The SchemaContext that will control the lifetime of the schema.
+   */
+  public async getSchema<T extends Schema>(key: SchemaKey, matchType: SchemaMatchType, context: SchemaContext): Promise<T | undefined> {
+    return this.getSchemaSync(key, matchType, context) as T;
+  }
+
+  /**
+   * Attempts to retrieve a Schema with the given SchemaKey by using the configured search paths
+   * to locate the XML Schema file from the file system. Returns only Schemas from XML files with
+   * their keys populated.
+   * @param key The SchemaKey of the Schema to retrieve.
+   * @param matchType The SchemaMatchType.
+   * @param context The SchemaContext that will control the lifetime of the schema.
+   */
+  public getSchemaSync<T extends Schema>(key: SchemaKey, matchType: SchemaMatchType, context: SchemaContext): T | undefined {
+    const candidates: FileSchemaKey[] = this.findEligibleSchemaKeys(key, matchType, "xml");
+
+    if (!candidates || candidates.length === 0)
+      return undefined;
+
+    const maxCandidate = candidates.sort(this.compareSchemaKeyByVersion)[candidates.length - 1];
+    const alias = this.getSchemaAlias(maxCandidate.schemaText!);
+    const schema = new Schema(context, maxCandidate, alias) as T;
+    context.addSchemaSync(schema);
+
+    this.addSchemaReferences(schema, context, matchType);
+    return schema;
+  }
+
+  /**
+   * Constructs a SchemaKey based on the information in the Schema XML.
+   * @param schemaXml The Schema XML as a string.
+   */
+  public getSchemaKey(schemaXml: string): SchemaKey {
+    const match = schemaXml.match(/<ECSchema.*schemaName="(?<name>\w+)".*version="(?<version>[\d|\.]+)"/) as any;
+    if (!match || !match.groups.name || !match.groups.version) {
+      throw new ECObjectsError(ECObjectsStatus.InvalidSchemaXML, `Could not find the ECSchema 'schemaName' or 'version' tag in the given file.`);
+    }
+
+    let ecVersion: ECVersion;
+    if (isECv2Schema(schemaXml))
+      ecVersion = SchemaXmlFileLocater.fromECv2String(match.groups.version);
+    else
+      ecVersion = ECVersion.fromString(match.groups.version);
+
+    const key = new SchemaKey(match.groups.name, ecVersion);
+    return key;
+  }
+
+  /**
+   * Gets an array of SchemaKeys of the Schemas referenced by the given Schema.
+   * @param xmlSchemaKey The SchemaKey of the parent Schema containing the references.
+   */
+  private getSchemaReferenceKeys(schemaKey: FileSchemaKey): SchemaKey[] {
+    return this._getSchemaReferenceKeys(schemaKey);
+  }
+
+  /**
+   * Adds schemas to the references collection for the given Schema by locating
+   * the referenced schemas.
+   * @param schema The schema for which to add the references.
+   * @param context The SchemaContext that will control the lifetime of the schema.
+   * @param refMatchType The SchemaMatchType to use when locating schema references.
+   */
+  private addSchemaReferences(schema: Schema, context: SchemaContext, refMatchType: SchemaMatchType): void {
+    const refKeys = this.getSchemaReferenceKeys(schema.schemaKey as FileSchemaKey);
+
+    for (const key of refKeys) {
+      const refSchema = context ? context.getSchemaSync(key, refMatchType) : undefined;
+      if (!refSchema)
+        throw new ECObjectsError(ECObjectsStatus.UnableToLocateSchema, `Unable to locate referenced schema: ${key.name}.${key.readVersion}.${key.writeVersion}.${key.minorVersion}`);
+
+      schema.references.push(refSchema);
+    }
+  }
+
+  /**
+   * Gets an array of SchemaKeys of the Schemas referenced by the given Schema.
+   * @param data The Schema XML string.
+   */
+  private _getSchemaReferenceKeys(xmlSchemaKey: FileSchemaKey): SchemaKey[] {
+    const file = xmlSchemaKey.schemaText;
+
+    if (!file)
+      throw new ECObjectsError(ECObjectsStatus.UnableToLocateSchema, `Could not locate the schema file, ${xmlSchemaKey.fileName}, for the schema ${xmlSchemaKey.name}`);
+
+    const data = file.toString().replace(/(\s*)<!--.*?-->/g, ""); // ignore any comments in the XML file when getting the array of SchemaKeys
+
+    const keys: SchemaKey[] = [];
+    const matches = data.match(/<ECSchemaReference ([^]+?)\/>/g);
+    if (!matches)
+      return keys;
+
+    for (const match of matches) {
+      const name = match.match(/name="(.+?)"/);
+      const versionMatch = match.match(/version="(.+?)"/);
+      if (!name || name.length !== 2 || !versionMatch || versionMatch.length !== 2)
+        throw new ECObjectsError(ECObjectsStatus.InvalidSchemaXML, `Invalid ECSchemaReference xml encountered in the schema file`);
+
+      // write version maybe missing, so insert "0"
+      let versionString = versionMatch[1];
+      const versionParts = versionString.split(".");
+      if (versionParts.length === 2)
+        versionParts.splice(1, 0, "0");
+
+      versionString = versionParts.join(".");
+
+      const key = new SchemaKey(name[1], ECVersion.fromString(versionString));
+      keys.push(key);
+    }
+
+    return keys;
+  }
+
+  /**
+   * Gets the Schema alias from the Schema XML.
+   * @param data The Schema XML as a string.
+   */
+  private getSchemaAlias(schemaXml: string): string {
+    const match = schemaXml.match(/<ECSchema.*alias="(?<alias>\w+)"/) as any;
+    if (!match || !match.groups.alias) {
+      throw new ECObjectsError(ECObjectsStatus.InvalidSchemaXML, `Could not find the ECSchema 'alias' tag in the given file.`);
+    }
+
+    return match.groups.alias;
   }
 }
