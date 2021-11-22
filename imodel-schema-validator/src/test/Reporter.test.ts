ec664d673824e9d2bcc1982582f1d43efdc2f2f9
christophermlawson <32881725+christophermlawson@users.noreply.github.com>
2021-11-22 10:08:36 -0500

    remove it.only

--- imodel-schema-validator/src/test/Reporter.test.ts
+++ imodel-schema-validator/src/test/Reporter.test.ts
@@ -36,7 +36,7 @@ describe("Reporter class tests.", () => {
     expect(result).to.equals(true);
   });
 
-  it.only("Log all validation results.", async () => {
+  it("Log all validation results.", async () => {
     const exportDir = path.resolve(path.normalize(__dirname + "/assets/"));
     const output = path.join(outputDir, "wip");
 
